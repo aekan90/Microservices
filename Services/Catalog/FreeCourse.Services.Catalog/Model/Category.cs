f94bcbd4f165ddfc11c61c5e8859ca0e191951a9
Atıf Ertugrul Kan <aekan90@hotmail.com>
2023-12-05 16:04:50 +0300

    Modelleri oluşturdum.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Model
+{
+	public class Category
+	{
+		[BsonId] // Id attributü
+		[BsonRepresentation(BsonType.ObjectId)] // Tipi de ID
+		public string Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Course.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FreeCourse.Services.Catalog.Model
+{
+	public class Course
+	{
+		[BsonId] // Id attributü
+		[BsonRepresentation(BsonType.ObjectId)] // Tipi de ID
+		public string Id { get; set; }
+		public string Name { get; set; }
+
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string CategoryId { get; set; }
+
+		[BsonIgnore] // mongoDb collection'a yansıtma
+        public Category Category { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+		public string Price { get; set; }
+		public string UserId { get; set; }
+
+		[BsonRepresentation(BsonType.Decimal128)]
+		public string Picture { get; set; }
+		public DateTime CreatedTime { get; set; }
+		public string Description { get; set; }
+		public Feature Feature { get; set; }
+	}
+}
